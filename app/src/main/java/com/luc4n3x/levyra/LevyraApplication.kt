3a7c9018e10fb2d327cea84d2cd35dc83c649938
LUC4N3X <luca.testing96@gmail.com>
2026-07-06 12:59:36 +0200

    Update LevyraApplication.kt

--- app/src/main/java/com/luc4n3x/levyra/LevyraApplication.kt
+++ app/src/main/java/com/luc4n3x/levyra/LevyraApplication.kt
@@ -1,6 +1,7 @@
 package com.luc4n3x.levyra
 
 import android.app.Application
+import com.luc4n3x.levyra.data.LevyraArtworkCache
 import com.luc4n3x.levyra.data.ReleaseRadarWorker
 import timber.log.Timber
 
@@ -8,6 +9,7 @@ class LevyraApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+        LevyraArtworkCache.configure(this)
         runCatching { ReleaseRadarWorker.schedule(this) }
             .onFailure { Timber.w(it, "Release radar scheduling failed") }
     }
