9f34b86e0b9140434ebaf4337d1d17e57d58bbcd
LUC4N3X <luca.testing96@gmail.com>
2026-07-12 13:54:33 +0200

    Remove unused import for matchParentSize

--- app/src/main/java/com/luc4n3x/levyra/ui/LevyraApp.kt
+++ app/src/main/java/com/luc4n3x/levyra/ui/LevyraApp.kt
@@ -64,7 +64,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.matchParentSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
