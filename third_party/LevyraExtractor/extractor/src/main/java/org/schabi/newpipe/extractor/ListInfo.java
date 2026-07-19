1dc35e1b567db427afbec293a523b70f23b6c117
LUC4N3X <luca.testing96@gmail.com>
2026-07-19 14:12:40 +0200

    Refactor ListInfo class and introduce ListExtractor

--- third_party/LevyraExtractor/extractor/src/main/java/org/schabi/newpipe/extractor/ListInfo.java
+++ third_party/LevyraExtractor/extractor/src/main/java/org/schabi/newpipe/extractor/ListInfo.java
@@ -1,62 +1,156 @@
 package org.schabi.newpipe.extractor;
 
-import org.schabi.newpipe.extractor.search.filter.FilterItem;
-
+import org.schabi.newpipe.extractor.exceptions.ExtractionException;
 import org.schabi.newpipe.extractor.linkhandler.ListLinkHandler;
-
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public abstract class ListInfo<T extends InfoItem> extends Info {
-    private List<T> relatedItems;
-    private Page nextPage = null;
-    private final List<FilterItem> contentFilters;
-    private final List<FilterItem> sortFilter;
+import javax.annotation.Nonnull;
 
-    public ListInfo(final int serviceId,
-                    final String id,
-                    final String url,
-                    final String originalUrl,
-                    final String name,
-                    final List<FilterItem> contentFilter,
-                    final List<FilterItem> sortFilter) {
-        super(serviceId, id, url, originalUrl, name);
-        this.contentFilters = contentFilter;
-        this.sortFilter = sortFilter;
+
+/**
+ * Base class to extractors that have a list (e.g. playlists, users).
+ * @param <R> the info item type this list extractor provides
+ */
+public abstract class ListExtractor<R extends InfoItem> extends Extractor {
+    /**
+     * Constant that should be returned whenever
+     * a list has an unknown number of items.
+     */
+    public static final long ITEM_COUNT_UNKNOWN = -1;
+    /**
+     * Constant that should be returned whenever a list has an
+     * infinite number of items. For example a YouTube mix.
+     */
+    public static final long ITEM_COUNT_INFINITE = -2;
+    /**
+     * Constant that should be returned whenever a list
+     * has an unknown number of items bigger than 100.
+     */
+    public static final long ITEM_COUNT_MORE_THAN_100 = -3;
+
+    public ListExtractor(final StreamingService service, final ListLinkHandler linkHandler) {
+        super(service, linkHandler);
     }
 
-    public ListInfo(final int serviceId,
-                    final ListLinkHandler listUrlIdHandler,
-                    final String name) {
-        super(serviceId, listUrlIdHandler, name);
-        this.contentFilters = listUrlIdHandler.getContentFilters();
-        this.sortFilter = listUrlIdHandler.getSortFilter();
+    /**
+     * A {@link InfoItemsPage InfoItemsPage} corresponding to the initial page
+     * where the items are from the initial request and the nextPage relative to it.
+     *
+     * @return a {@link InfoItemsPage} corresponding to the initial page
+     */
+    @Nonnull
+    public abstract InfoItemsPage<R> getInitialPage() throws IOException, ExtractionException;
+
+    @Nonnull
+    public InfoItemsPage<R> getFullPage() throws IOException, ExtractionException {
+        InfoItemsPage<R> currentPage = getInitialPage();
+        final ArrayList<R> items = new ArrayList<>(currentPage.getItems());
+        final ArrayList<Throwable> errors = new ArrayList<>(currentPage.getErrors());
+        while (currentPage.hasNextPage()) {
+            currentPage = getPage(currentPage.getNextPage());
+            items.addAll(currentPage.getItems());
+            errors.addAll(currentPage.getErrors());
+        }
+        return new InfoItemsPage<>(items, null, errors);
     }
 
-    public List<T> getRelatedItems() {
-        return relatedItems;
+    /**
+     * Get a list of items corresponding to the specific requested page.
+     *
+     * @param page any page got from the exclusive implementation of the list extractor
+     * @return a {@link InfoItemsPage} corresponding to the requested page
+     * @see InfoItemsPage#getNextPage()
+     */
+    public abstract InfoItemsPage<R> getPage(Page page) throws IOException, ExtractionException;
+
+    @Nonnull
+    @Override
+    public ListLinkHandler getLinkHandler() {
+        return (ListLinkHandler) super.getLinkHandler();
     }
 
-    public void setRelatedItems(final List<T> relatedItems) {
-        this.relatedItems = relatedItems;
-    }
+    /*//////////////////////////////////////////////////////////////////////////
+    // Inner
+    //////////////////////////////////////////////////////////////////////////*/
 
-    public boolean hasNextPage() {
-        return Page.isValid(nextPage);
-    }
+    /**
+     * A class that is used to wrap a list of gathered items and eventual errors, it
+     * also contains a field that points to the next available page ({@link #nextPage}).
+     * @param <T> the info item type that this page is supposed to store and provide
+     */
+    public static class InfoItemsPage<T extends InfoItem> {
+        /**
+         * A convenient method that returns a representation of an empty page.
+         *
+         * @return a type-safe page with the list of items and errors empty and the nextPage set to
+         *         {@code null}.
+         */
+        public static <T extends InfoItem> InfoItemsPage<T> emptyPage() {
+            return new InfoItemsPage<>(Collections.emptyList(), null, Collections.emptyList());
+        }
 
-    public Page getNextPage() {
-        return nextPage;
-    }
+        /**
+         * The current list of items of this page
+         */
+        private final List<T> itemsList;
 
-    public void setNextPage(final Page page) {
-        this.nextPage = page;
-    }
+        /**
+         * Url pointing to the next page relative to this one
+         *
+         * @see ListExtractor#getPage(Page)
+         * @see Page
+         */
+        private final Page nextPage;
 
-    public List<FilterItem> getContentFilters() {
-        return contentFilters;
-    }
+        /**
+         * Errors that happened during the extraction
+         */
+        private final List<Throwable> errors;
 
-    public List<FilterItem> getSortFilter() {
-        return sortFilter;
+        public InfoItemsPage(final InfoItemsCollector<T, ?> collector, final Page nextPage) {
+            this(collector.getItems(), nextPage, collector.getErrors());
+        }
+
+        public InfoItemsPage(final List<T> itemsList,
+                             final Page nextPage,
+                             final List<Throwable> errors) {
+            this(itemsList, nextPage, errors, false);
+        }
+
+        /**
+         * Creates a page while optionally preserving a valid continuation when no items were
+         * successfully parsed. This is useful for feeds where a legitimate page can contain only
+         * unavailable, deleted or otherwise unsupported renderers while later pages remain valid.
+         */
+        public InfoItemsPage(final List<T> itemsList,
+                             Page nextPage,
+                             final List<Throwable> errors,
+                             final boolean preserveNextPageWhenEmpty) {
+            if (!preserveNextPageWhenEmpty && itemsList.isEmpty()) {
+                nextPage = null;
+            }
+            this.itemsList = itemsList;
+            this.nextPage = nextPage;
+            this.errors = errors;
+        }
+
+        public boolean hasNextPage() {
+            return Page.isValid(nextPage);
+        }
+
+        public List<T> getItems() {
+            return itemsList;
+        }
+
+        public Page getNextPage() {
+            return nextPage;
+        }
+
+        public List<Throwable> getErrors() {
+            return errors;
+        }
     }
 }
