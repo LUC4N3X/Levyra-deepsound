682fc39a9d3f04fca7db221fd9ac21593cec7ea0
LUC4N3X <luca.testing96@gmail.com>
2026-08-03 16:12:13 +0200

    test(ui): cover font preset normalization

---
+++ app/src/test/java/com/luc4n3x/levyra/domain/LevyraFontPresetTest.kt
@@ -0,0 +1,23 @@
+package com.luc4n3x.levyra.domain
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class LevyraFontPresetTest {
+
+    @Test
+    fun parsesPresetNamesIgnoringCaseAndWhitespace() {
+        assertEquals(LevyraFontPreset.Manrope, LevyraFontPreset.from("  manROPE "))
+        assertEquals(LevyraFontPreset.Montserrat, LevyraFontPreset.from("Montserrat"))
+    }
+
+    @Test
+    fun unknownPresetFallsBackToOutfit() {
+        assertEquals(LevyraFontPreset.Outfit, LevyraFontPreset.from("not-a-font"))
+    }
+
+    @Test
+    fun systemPresetDoesNotRequireGoogleFonts() {
+        assertEquals(null, LevyraFontPreset.System.googleFontName)
+    }
+}
