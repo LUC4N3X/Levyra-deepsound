269caa553ccddc2846cdd8200b1477e6e8f83efa
LUC4N3X <luca.testing96@gmail.com>
2026-08-09 21:59:19 +0200

    chore: keep playback stabilization scoped

--- app/src/main/java/com/luc4n3x/levyra/domain/ReleaseType.kt
+++ app/src/main/java/com/luc4n3x/levyra/domain/ReleaseType.kt
@@ -43,8 +43,8 @@ fun releaseTypeFromProviderLabel(value: String): ReleaseType {
 }
 
 private val ALBUM_LABELS = normalizedLabelSet(
-    "album", "albumo", "alben", "albom", "albumes", "albumi", "άλμπουμ", "альбом",
-    "专辑", "專輯", "アルバム", "앨범", "एल्बम", "अल्बम", "อัลบั้ม", "אלבום", "ألبوم"
+    "album", "albumo", "alben", "albom", "albumes", "albumi", "专辑", "專輯", "アルバム", "앨범",
+    "अल्बम", "อัลบั้ม", "אלבום", "ألبوم"
 )
 
 private val SINGLE_LABELS = normalizedLabelSet(
