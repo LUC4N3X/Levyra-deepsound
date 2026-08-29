1933c03b490386d49832da5edb8af6a30dd481e9
LUC4N3X <luca.testing96@gmail.com>
2026-08-29 23:47:04 +0200

    refactor(motion): keep Apple script parsing with provider

--- app/src/main/java/com/luc4n3x/levyra/feature/motion/AppleMotionArtworkScriptPatterns.kt
+++
@@ -1,9 +0,0 @@
-package com.luc4n3x.levyra.feature.motion
-
-internal val SCRIPT_SRC_REGEX = Regex(
-    """(?i)<script\b[^>]*\bsrc\s*=\s*[\"']([^\"']+\.js(?:\?[^\"']*)?)[\"']"""
-)
-
-internal val LEGACY_SCRIPT_REGEX = Regex(
-    "[\\\"']([^\\\"']*/assets/index[^\\\"']*\\.js)[\\\"']"
-)
