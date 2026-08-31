2d23e14ffc11eb71fe99233cfa0236755e880683
LUCA <luca.testing96@gmail.com>
2026-08-31 20:22:35 +0200

    fix: align recognition toolchain for F-Droid

--- levyra-recognition/build.gradle.kts
+++ levyra-recognition/build.gradle.kts
@@ -2,8 +2,12 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
 }
 
+val isFdroidBuild = providers.gradleProperty("levyraFdroidBuild")
+    .map(String::toBoolean)
+    .getOrElse(false)
+
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(if (isFdroidBuild) 21 else 17)
 }
 
 dependencies {
