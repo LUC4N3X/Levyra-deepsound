de2a2822d373c174ef5807f971ce0b9ccc180f2f
LUC4N3X <luca.testing96@gmail.com>
2026-09-20 23:53:18 +0200

    test(i18n): cover ReplayGain 2.0 copy

---
+++ app/src/test/java/com/luc4n3x/levyra/ui/i18n/ReplayGainLocalizationTest.kt
@@ -0,0 +1,28 @@
+package com.luc4n3x.levyra.ui.i18n
+
+import com.luc4n3x.levyra.domain.ReplayGainMode
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class ReplayGainLocalizationTest {
+    @Test
+    fun everySupportedLanguageHasUsableReplayGainCopy() {
+        LevyraStrings.all().forEach { strings ->
+            val copy = strings.replayGainCopy()
+            assertTrue(strings.code, copy.track.isNotBlank())
+            assertTrue(strings.code, copy.album.isNotBlank())
+            assertTrue(strings.code, copy.smart.isNotBlank())
+            assertTrue(strings.code, copy.clippingProtection.isNotBlank())
+            assertTrue(strings.code, copy.peakAware.contains("ReplayGain", ignoreCase = true))
+        }
+    }
+
+    @Test
+    fun italianModeLabelsAreLocalized() {
+        val copy = LevyraStrings.forCode("it").replayGainCopy()
+        assertEquals("Traccia", copy.modeLabel(ReplayGainMode.TRACK))
+        assertEquals("Album", copy.modeLabel(ReplayGainMode.ALBUM))
+        assertEquals("Smart", copy.modeLabel(ReplayGainMode.SMART))
+    }
+}
