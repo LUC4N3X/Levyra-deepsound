64c483335165701801ca7d89241dbc6982d2e56a
LUC4N3X <luca.testing96@gmail.com>
2026-09-22 12:27:14 +0200

    chore(settings): remove obsolete layout import

--- app/src/main/java/com/luc4n3x/levyra/MainActivity.kt
+++ app/src/main/java/com/luc4n3x/levyra/MainActivity.kt
@@ -28,7 +28,6 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
